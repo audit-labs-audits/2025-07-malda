ca58a64f12cc0e8c895a125bf214e668a1bac645
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2025-07-24 12:18:04 +0200

    Delete scope directories

--- malda-lending/script/deployment/markets/BaseMarketDeploy.s.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: BSL-1.1
-pragma solidity =0.8.28;
-
-import {Script, console} from "forge-std/Script.sol";
-import {DeployBase} from "script/deployers/DeployBase.sol";
-
-abstract contract BaseMarketDeploy is Script, DeployBase {}
